45a167b25d7d89a738ca2bbbb2a02559cebd6cc9
samarth2534@gmail.com <samarth2534@gmail.com>
2026-02-23 20:37:10 +0530

    Made auth necessary for navigation

--- chat-web/src/components/Body.jsx
+++ chat-web/src/components/Body.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
   const fetchUser = async () => {
+    if(userData) return;
     try {
       const res = await axios.get(BASE_URL + "profile/profile", {
         withCredentials: true,
       });
       dispatch(addUser(res.data));
     } catch (err) {
+      if (err.status == 401) {
+        navigate("/login");
+      }
+      //create a error page
       console.error(err);
     }
   };
 
   useEffect(() => {
-    fetchUser();
+      fetchUser();
   }, []);
 
   return (
